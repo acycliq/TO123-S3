95ff47f541fd68bcdd49aaea07a91e9485833ac8
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-08-04 11:49:20 +0100

    minor edit

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -2,7 +2,7 @@ function config() {
     return {
             roi: {"x0": 0, "x1": 13293, "y0": 0, "y1":7826},
             imageSize: [262144, 154332],
-            tiles: 'https://raw.githubusercontent.com/acycliq/TO123-S3/master/dashboard/data/img/262144/{z}/{y}/{x}.jpg',
+            tiles: 'https://raw.githubusercontent.com/acycliq/TO123-S3/master/dashboard/data/img/262144px/{z}/{y}/{x}.jpg',
             cellData: 'https://api.github.com/repos/acycliq/TO123-S3/contents/dashboard/data/cellData',
             geneData: 'https://api.github.com/repos/acycliq/TO123-S3/contents/dashboard/data/geneData',
             cellCoords: 'https://api.github.com/repos/acycliq/TO123-S3/contents/dashboard/data/cellCoords',
